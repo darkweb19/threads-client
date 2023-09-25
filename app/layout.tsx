444fb38bd201f64d3c515ae14b415ed5af4731aa
WooFoo <sujanstha3220@gmail.com>
2023-09-25 18:38:35 +0545

    tanstack query provider is provided

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 "use client";
 import "./globals.css";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Inter } from "next/font/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const inter = Inter({ subsets: ["latin"] });
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
 	children,
@@ -15,10 +17,13 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<GoogleOAuthProvider clientId="1013886399741-ocsv3peh6c4pvvjfr34v9q141ueme8kg.apps.googleusercontent.com">
-					{children}
-					<Toaster />
-				</GoogleOAuthProvider>
+				<QueryClientProvider client={queryClient}>
+					<GoogleOAuthProvider clientId="1013886399741-ocsv3peh6c4pvvjfr34v9q141ueme8kg.apps.googleusercontent.com">
+						{children}
+						<Toaster />
+						<ReactQueryDevtools />
+					</GoogleOAuthProvider>
+				</QueryClientProvider>
 			</body>
 		</html>
 	);
