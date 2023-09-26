6f9a8834d27a190a2698b2a484398708960c743c
WooFoo <sujanstha3220@gmail.com>
2023-09-26 15:50:27 +0545

    user image is added on headingPost

--- components/feedcard.tsx
+++ components/feedcard.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { FaRegComment, FaRetweet } from "react-icons/fa6";
 import { AiOutlineHeart } from "react-icons/ai";
--- components/headingPost.tsx
+++ components/headingPost.tsx
@@ -1,15 +1,21 @@
+"use client";
+import { useCurrentUser } from "@/hooks/user";
 import Image from "next/image";
 export default function HeadingPost() {
+	const { user } = useCurrentUser();
+
 	return (
 		<div className="flex flex-row w-full gap-2 border-b border-gray-800 px-2 py-4 items-center justify-between">
 			<div className="flex gap-3 items-center text-gray-500">
-				<Image
-					src={"https://avatars.githubusercontent.com/u/61812195?v=4"}
-					className="rounded-full"
-					alt="profile image"
-					width={40}
-					height={40}
-				/>
+				{user?.getCurrentUser?.profileImageUrl && (
+					<Image
+						src={user?.getCurrentUser?.profileImageUrl}
+						className="rounded-full"
+						alt="profile image"
+						width={40}
+						height={40}
+					/>
+				)}
 				<input
 					type="text"
 					placeholder="Start a thread..."
@@ -17,7 +23,7 @@ export default function HeadingPost() {
 				/>
 			</div>
 
-			<div className="border cursor-not-allowed border-gray-500 flex text-gray-500 h-fit rounded-lg items-center w-fit  px-3 py-1 ">
+			<div className="border cursor-pointer border-gray-500 flex text-gray-500 h-fit rounded-lg items-center w-fit  px-3 py-1 ">
 				Post
 			</div>
 		</div>
