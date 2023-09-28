518e3abec67b806169c0b6b3c72a70fa092e3b5d
WooFoo <sujanstha3220@gmail.com>
2023-09-28 13:13:54 +0545

    a query for creating a thread

--- hooks/thread.ts
+++ hooks/thread.ts
@@ -1,10 +1,28 @@
 import { graphqlClient } from "@/clients/api";
+import { CreateThreadData } from "@/gql/graphql";
+import { createThreadMutation } from "@/graphql/mutation/thread";
 import { getAllThreadsQuery } from "@/graphql/query/thread";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useCreateThread = () => {
+	const queryClient = useQueryClient();
+	const mutation = useMutation({
+		mutationFn: (payload: CreateThreadData) =>
+			graphqlClient.request(createThreadMutation, { payload }),
+		onMutate: () => toast.loading("Creating Tweet", { id: "1" }),
+		onSuccess: async () => {
+			await queryClient.invalidateQueries(["all-threads"]);
+			toast.success("Thread Created", { id: "1" });
+		},
+	});
+
+	return mutation;
+};
 
 export const useGetAllThreads = () => {
 	const query = useQuery({
-		queryKey: ["all-tweets"],
+		queryKey: ["all-threads"],
 		queryFn: () => graphqlClient.request(getAllThreadsQuery),
 	});
 	return { ...query, threads: query.data?.getAllThreads };
